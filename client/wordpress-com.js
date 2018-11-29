4183f622d0e05663aeae30f0868deb6e1800c16a
artpi <artur.piszek@gmail.com>
2018-11-29 15:53:01 +0100

    Reorganization

--- client/wordpress-com.js
+++ client/wordpress-com.js
@@ -266,9 +266,9 @@ const sections = [
 		enableLoggedOut: true,
 	},
 	{
-		name: 'ads',
-		paths: [ '/ads' ],
-		module: 'my-sites/ads',
+		name: 'earn',
+		paths: [ '/earn' ],
+		module: 'my-sites/earn',
 		secondary: true,
 		group: 'sites',
 	},
